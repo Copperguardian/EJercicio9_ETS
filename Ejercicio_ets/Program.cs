a8add7f78419ab8689fdb511713e20654499acd0
Hugo Ramos Almenar <hugoramosalmenar@gmail.com>
2022-12-01 14:39:36 +0100

    1º Commit

---
+++ Ejercicio_ets/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace programa{
+    class program{
+        static void Main(string[] args){
+            //Ahora zorras
+            int a = 0;
+            int b = 0;
+            Console.WriteLine("Bovinos");
+
+
+
+
+
+
+
+        }
+    }
+}
